28c0476fa89c673d77539ed276e602fc7e8a18d2
Vacuda <adamjsikora@gmail.com>
2020-01-10 16:43:30 -0600

    First Commit

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+// import { TaskService } from './task.service';
+import { FormsModule } from '@angular/forms';
+// import { ShowComponent } from './show/show.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // ShowComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+//   providers: [TaskService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
